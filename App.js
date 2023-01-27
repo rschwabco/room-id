460178d780d0369ee25d9e767f00195f48403bff
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-01-27 10:15:55 -0800

    Working

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { Camera, CameraType } from "expo-camera";
 import { useState, useEffect, useRef } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
 import io from "socket.io-client";
 import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
+import IconButton from "./components/IconButton";
+import { Switch } from "@rneui/themed";
+import { TaskTimer } from "tasktimer";
 
 export default function App() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [cameraReady, setCameraReady] = useState(false);
   const [recording, setRecording] = useState(false);
-  const [hasConnection, setConnection] = useState(true);
-  const ENDPOINT = "https://public-deer-start-97-113-152-235.loca.lt";
+  const [hasConnection, setConnection] = useState(false);
+  const [detectedLabel, setDetectedLabel] = useState(null);
+  const ENDPOINT = "https://neat-tips-give-97-113-152-235.loca.lt";
   const interval = 2000;
+  const [label, setLabel] = useState(null);
+  const [training, setTraining] = useState(false);
+  const [intervalId, setIntervalID] = useState(0);
 
-  let intervalId = useRef(null);
   let socket = useRef(null);
   const cameraRef = useRef(null);
 
@@ -23,31 +36,56 @@ export default function App() {
         transports: ["websocket"],
       });
 
-      socket.current.io.on("open", () => setConnection(true));
+      socket.current.on("connect", () => {
+        console.log("connected");
+        setConnection(true);
+      });
+
+      socket.current.on("detectedLabel", (label) => {
+        setDetectedLabel(label);
+      });
+
       socket.current.io.on("close", () => setConnection(false));
 
+      socket.current.on("disconnect", () => {
+        setConnection(false);
+      });
+
       return function didUnmount() {
-        // socket.current.disconnect();
-        // socket.current.removeAllListeners();
+        socket.current.disconnect();
+        socket.current.removeAllListeners();
       };
     },
     [ENDPOINT]
   );
 
   useEffect(() => {
-    console.log("recording", recording);
+    console.log("intervalId", intervalId);
     if (!cameraReady) {
+      console.log("camera not ready");
       return;
-    }
-    if (recording && !intervalId.current) {
-      intervalId.current = setInterval(takePicture, interval);
     } else {
-      if (!recording && intervalId.current) {
-        console.log("stopped", recording, intervalId.current);
-        clearInterval(intervalId.current);
+      console.log("camera ready", recording, intervalId);
+      if (recording && !intervalId) {
+        console.log("started", recording, intervalId);
+        setIntervalID(setInterval(takePicture, interval));
+      } else {
+        if (!recording && intervalId) {
+          console.log("stopped", recording, intervalId);
+          clearInterval(intervalId);
+          setIntervalID(null);
+        }
       }
     }
   }, [recording]);
+
+  useEffect(() => {
+    if (training) {
+      setDetectedLabel("Training");
+    } else {
+      setDetectedLabel("");
+    }
+  }, [training]);
 
   if (!permission) {
     // Camera permissions are still loading
@@ -75,9 +113,11 @@ export default function App() {
     });
     const resizedPic = await manipulateAsync(
       pic.uri,
-      [{ resize: { width: pic.width / 10, height: pic.height / 10 } }],
+      [{ resize: { width: 244, height: 244 } }],
       { compress: 1, format: SaveFormat.JPEG, base64: true }
     );
+
+    console.log("HAS CONNECTION", hasConnection);
 
     hasConnection &&
       socket.current.emit("picture", {
@@ -85,7 +125,12 @@ export default function App() {
         data: resizedPic.base64,
         height: resizedPic.height,
         width: resizedPic.width,
+        label,
+        stage: training ? "training" : "querying",
       });
+
+    hasConnection &&
+      console.log("sending picture", resizedPic.width, resizedPic.height);
   }
 
   function toggleRecording() {
@@ -100,32 +145,60 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {!hasConnection && (
+      {/* {!hasConnection && (
         <>
           <Text style={styles.paragraph}>Connecting to {ENDPOINT}...</Text>
           <Text style={styles.footnote}>
             Make sure the backend is started and reachable
           </Text>
         </>
-      )}
+      )} */}
 
-      {hasConnection && (
+      {
         <Camera
           style={styles.camera}
           type={type}
           onCameraReady={() => setCameraReady(true)}
           ref={cameraRef}
         >
+          <View style={styles.controls}>
+            {training && (
+              <TextInput
+                value={label}
+                onChangeText={(label) => setLabel(label)}
+                placeholder={"Label"}
+                style={styles.input}
+              />
+            )}
+          </View>
+          <Text style={styles.detectedLabel}>
+            {hasConnection ? (detectedLabel ? detectedLabel : "Unknown") : ""}
+          </Text>
           <View style={styles.buttonContainer}>
             <TouchableOpacity
               style={styles.button}
               onPress={() => toggleRecording()}
+              disabled={!hasConnection}
             >
-              <Text style={styles.text}>Rec</Text>
+              <Text
+                style={{
+                  ...styles.text,
+                  color: recording ? "red" : hasConnection ? "white" : "grey",
+                }}
+              >
+                Rec
+              </Text>
+              <Switch
+                value={training}
+                onValueChange={(value) => setTraining(value)}
+              />
+              <Text style={{ fontSize: 10, color: "white" }}>
+                {hasConnection ? "Connected" : "Disconnected"}
+              </Text>
             </TouchableOpacity>
           </View>
         </Camera>
-      )}
+      }
     </View>
   );
 }
@@ -138,6 +211,23 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
   },
+  controls: {
+    paddingTop: 70,
+    backgroundColor: "transparent",
+    display: "flex",
+    flexDirection: "column",
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  detectedLabel: {
+    flex: 1,
+    alignContent: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
   buttonContainer: {
     flex: 1,
     flexDirection: "row",
@@ -149,9 +239,20 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
     alignItems: "center",
   },
+  input: {
+    flex: 1,
+    flexDirection: "column",
+    width: 250,
+    height: 44,
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    backgroundColor: "#e8e8e8",
+  },
   text: {
     fontSize: 24,
     fontWeight: "bold",
     color: "white",
+    marginBottom: 20,
   },
 });
